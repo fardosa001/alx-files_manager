d4bdd602362c28a87d8393a016440c551cf15bf6
fardosa001 <fardosaadow@gmail.com>
2024-05-14 22:00:32 +0300

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,41 +1,34 @@
 const { MongoClient } = require('mongodb');
 
-
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        
-        const url = `mongodb://${host}:${port}`;
-        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    }
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
 
-    async connect() {
-        try {
-            await this.client.connect();
-            console.log('Connected to MongoDB');
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    const url = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
 
-    async isAlive() {
-        return this.client.isConnected();
-    }
+  async isAlive() {
+    return this.connected;
+  }
 
-    async nbUsers() {
-        const usersCollection = this.client.db().collection('users');
-        return usersCollection.countDocuments();
-    }
+  async nbUsers() {
+    const usersCollection = this.client.db(this.database).collection('users');
+    return usersCollection.countDocuments();
+  }
 
-    async nbFiles() {
-        const filesCollection = this.client.db().collection('files');
-        return filesCollection.countDocuments();
-    }
+  async nbFiles() {
+    const filesCollection = this.client.db(this.database).collection('files');
+    return filesCollection.countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
-dbClient.connect();
 
 module.exports = dbClient;
